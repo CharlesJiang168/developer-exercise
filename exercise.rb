2d6503539b13a8fd9e8e7fd01bb0ce93f4148d2a
charlesadam <charlesadam@Charless-MacBook-Pro.local>
2019-02-10 18:31:09 -0800

    Two questions resolved

--- exercise.rb
+++ exercise.rb
@@ -4,15 +4,60 @@ class Exercise
   # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
   def self.marklar(str)
-    # TODO: Implement this method
+    newWords = []
+    # words = str.split()
+    words = str.split(/(\.|\?|\!|\W+)/)
+    # print "#{words}"
+    for word in words
+      # puts "#{word}"
+      if word.length > 4
+        if word[0] == word[0].upcase
+          newWords.push("Marklar")
+        else
+          newWords.push("marklar")
+        end
+      else
+        newWords.push(word)
+      end
+    end
+    return newWords.join("")
   end
 
+  def self.fibonacci(n)
+    # if n = 1, we want to output 1
+    if n == 1
+      1
+    # if n = 2, we want to output 1
+    elsif n == 2
+      1
+    # if n > 2, we want to output the sum of the previous two values
+    else
+      fibonacci(n - 1) + fibonacci(n - 2)
+    end
+  end
+
+  # puts "fibonacci: #{fibonacci(2)}"
   # Return the sum of all even numbers in the Fibonacci sequence, up to
   # the "nth" term in the sequence
   # eg. the Fibonacci sequence up to 6 terms is (1, 1, 2, 3, 5, 8),
   # and the sum of its even numbers is (2 + 8) = 10
   def self.even_fibonacci(nth)
-    # TODO: Implement this method
-  end
+
+    a=[]; a[0]=1; a[1]=1;
+    i = 1
+    nthFibonacci = fibonacci(nth)
+    while a[-1] < nthFibonacci
+      a[i+1]=(a[i] + a[i-1])
+      i +=1
+    end
+    x = 0
+    a.each do |num|
+      if num % 2 == 0
+        x += num
+      end
+    end
+    return x
+ end
 
 end
+
